85ed8d830268eae68bd40c8571ccb882e97d060e
Abhishek <akmahato2003@gmail.com>
2022-11-10 21:14:34 +0530

    version 1

---
+++ main.asm
@@ -0,0 +1,26 @@
+ORG 00H
+MOV A, #55H
+AGAIN: MOV P1, A
+ACALL DELAY
+CPL A
+SJMP AGAIN
+;_________delay subroutine
+/*
+ORG 30H
+DELAY:
+MOV R2, #100
+HERE1: MOV R3, #255
+HERE2: DJNZ R3, HERE2
+       DJNZ R2, HERE1
+	   RET
+	   END
+	   */
+	   DELAY:
+	   MOV R2, #86
+	   HERE1: MOV R1, #255
+	   HERE2: MOV R0, #255
+	   HERE3: DJNZ R0, HERE3
+	   DJNZ R1, HERE2
+	   DJNZ R2, HERE1
+	   RET
+	   END
